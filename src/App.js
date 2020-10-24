8d53900bb4cf02ad357499c6a75801e0a9f3b395
Jean Carlos  Ramos <rmscrlos@gmail.com>
2020-10-24 12:28:18 -0400

    some buttons to manipulates date through state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Showcase from './components/showcase/Showcase';
 import Speakers from './components/speakers/Speakers';
+import Schedule from './components/schedule/Schedule';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Showcase />
       <Speakers />
+      <Schedule />
     </div>
   );
 }
